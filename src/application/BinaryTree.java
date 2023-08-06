9fef61c095de5d38b16d06316a7559f0910e0ab4
Anna <anna.chymshyt@gmail.com>
2023-08-06 22:28:33 +0300

    Add nodes

--- src/application/BinaryTree.java
+++ src/application/BinaryTree.java
@@ -8,6 +8,19 @@ class BinaryTree {
 
         BinaryTree tree = new BinaryTree();
 
+        tree.root = new Node(7);
+        tree.root.left = new Node(10);
+        tree.root.right = new Node(8);
+        tree.root.left.left = new Node(3);
+        tree.root.left.right = new Node(1);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(25);
+        tree.root.left.left.left = new Node(14);
+        tree.root.left.left.right = new Node(4);
+        tree.root.left.right.left = new Node(18);
+        tree.root.left.right.right = new Node(11);
+        tree.root.right.left.left = new Node(20);
+
     }
 
     BinaryTree() {
