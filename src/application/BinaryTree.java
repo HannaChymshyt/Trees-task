b06af061ada7d83876a15f6a1d9a24a768e92d3c
Anna <anna.chymshyt@gmail.com>
2023-08-06 22:18:53 +0300

    Initial commit

---
+++ src/application/BinaryTree.java
@@ -0,0 +1,17 @@
+package application;
+
+class BinaryTree {
+
+    Node root;
+
+    public static void main(String[] args) {
+
+        BinaryTree tree = new BinaryTree();
+
+    }
+
+    BinaryTree() {
+        root = null;
+    }
+
+}
